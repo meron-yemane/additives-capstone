418f101f88675d3ceab7403f77df021e3e5c1cf7
meron93 <meronyemane18@gmail.com>
2017-02-28 20:53:55 -0800

    api error updated to 301

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var eAdditivesEndPoint = "https://vx-e-additives.p.mashape.com/additives?callback";
+var eAdditivesEndPoint = "https://vx-e-additives.p.mashape.com/additives/";
+var ep = "http://e-additives.vexelon.net/api/additives/search/"; // url from list of url's in console
+var ep2 = "http://e-additives.vexelon.net/api"; //api endpoint for the eadditives website from progammableweb.com
 
 var displaySearchData = function(data) {
   console.log("aklsdjf")
@@ -11,20 +13,20 @@ var jsonCallback = function(json) {
   console.log(json);
 };
 
-var getDatafromAPI = function (searchTerm, callback) {
+var getDatafromAPI = function(searchTerm, callback) {
   $.ajax({
     dataType: "jsonp",
-    url: eAdditivesEndPoint,
-    //jsonpCallback: "jsonCallback",
+    url: ep2,
+    //jsonpCallback: jsonCallback,
     data: {
       q: searchTerm
     },
     beforeSend: function(req) {
       req.setRequestHeader("User-Agent", "abc");
       req.setRequestHeader("Access-Control-Allow-Origin", "*");
-      req.setRequestHeader("X-Mashape-Key", "kfb2kuNb7GmshV5WqaO2PFeGdYzOp1klnZCjsnvbij8k2iko8Y");
+      req.setRequestHeader("X-Mashape-Key", "prQtUj9aZMmshk74S0jaHQgyiYyTp1QOtU6jsnke0pjORjGkr2"); // produciton key obtained from mashape.com(former string was the testing key)
       req.setRequestHeader("Accept", "application/json");
-      req.setRequestHeader("X-Authorization", "EAD-TOKENS apiKey=kfb2kuNb7GmshV5WqaO2PFeGdYzOp1klnZCjsnvbij8k2iko8Y");
+      req.setRequestHeader("X-Authorization", "EAD-TOKENS apiKey=prQtUj9aZMmshk74S0jaHQgyiYyTp1QOtU6jsnke0pjORjGkr2");  // produciton key obtained from mashape.com(former string was the testing key)
     },
     success: function(data) {
       console.log(data);
